914990ee10d4f868eae0b0472cc68e6924f1dbfd
elakia <vmelakia@gmail.com>
2022-02-25 23:53:42 +0530

    create basic file

---
+++ templates.py
@@ -0,0 +1,21 @@
+'''
+Created on 
+
+Course work: 
+
+@author: Elakia VM
+
+Source:
+    
+'''
+
+# Import necessary modules
+ 
+
+
+def startpy():
+    pass
+
+
+if __name__ == '__main__':
+    startpy()
